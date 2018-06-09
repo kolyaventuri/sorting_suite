3cbd48e79fd22430ca99c62bc162647bf5da3579
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-09 09:16:39 -0600

    QuickSort stores array

--- test/quicksort_test.js
+++ test/quicksort_test.js
@@ -2,10 +2,14 @@ require('./test_helper');
 const QuickSort = require('../lib/quicksort');
 
 describe('QuickSort', () => {
-  let quickSort = new QuickSort();
+  it('should be able to store an array', () => {
+    let quickSort = new QuickSort([1,2]);
+    expect(quickSort.array).to.be.an('array').that.eqls([1,2]);
+  });
 
   it('should be able to select a pivot', () => {
-    let pivot = quickSort.pivot([1,3,5,4,2]);
+    let quickSort = new QuickSort([1,3,5,4,2]);
+    let pivot = quickSort.pivot();
 
     expect(pivot).to.have.property('index').that.equals(1);
     expect(pivot).to.have.property('value').that.equals(3);
